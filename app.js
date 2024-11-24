8569efdc46c0a2a88076b1b2c643a5b25a4f9145
drawar250 <129526967+drawar250@users.noreply.github.com>
2024-11-24 11:19:17 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
-
+const firebaseConfig = {
+  apiKey: "AIzaSyCZkKqoN5pqg__FoQqj1TCMTn-MC6vnx_M",
+  authDomain: "realtime-ec549.firebaseapp.com",
+  projectId: "realtime-ec549",
+  storageBucket: "realtime-ec549.firebasestorage.app",
+  messagingSenderId: "769438819719",
+  appId: "1:769438819719:web:c97b4c3fdbe205a693df22"
+};
